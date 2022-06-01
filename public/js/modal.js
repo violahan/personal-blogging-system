9bf3f1adba2a238d56d7a803ffe85e38ade421b0
Viola Han <hanjiaqi0122@gmail.com>
2022-06-01 13:08:33 +1200

    fixed the login bug

--- public/js/modal.js
+++ public/js/modal.js
@@ -8,9 +8,12 @@ var loginBtn = document.getElementById("loginBtn");
 var loginClose = document.getElementById("loginClose");
 
 // When the user clicks the button, open the modal
-loginBtn.onclick = function() {
-    loginModal.style.display = "block";
+if (loginBtn){
+    loginBtn.onclick = function() {
+        loginModal.style.display = "block";
+    }
 }
+
 
 // When the user clicks on <span> (x), close the modal
 loginClose.onclick = function() {
