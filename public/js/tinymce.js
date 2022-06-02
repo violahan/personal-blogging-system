62fbb6c144126d974a093e0500b96f50aa601afa
Marshall Whitehouse <mwhi322@aucklanduni.ac.nz>
2022-06-03 09:25:57 +1200

    Added error text warnings to create and edit article

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -20,9 +20,24 @@ tinymce.init({
       let submitButton = document.getElementById('create-article-button')
 
       if (articleTitleContent == "" || articleContent == ""){
+        
+        if(articleTitleContent == ""){
+          document.getElementById('article-title-error').removeAttribute("hidden");
+        } else {
+          document.getElementById('article-title-error').setAttribute("hidden", true);
+        }
+
+        if(articleContent == ""){
+          document.getElementById('article-content-error').removeAttribute("hidden");
+        } else {
+          document.getElementById('article-content-error').setAttribute("hidden", true);
+        }
+
         submitButton.disabled = true;
       } else {
         submitButton.disabled = false;
+        document.getElementById('article-title-error').setAttribute("hidden", true);
+        document.getElementById('article-content-error').setAttribute("hidden", true);
       }
 
     });
